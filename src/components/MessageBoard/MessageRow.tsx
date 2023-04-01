48b97a2b3786141028441adb78a76cedffd035d7
Andrey Ilin <neform@gmail.com>
2023-04-02 01:30:23 +0200

    refactor: simplified message row

--- src/components/MessageBoard/MessageRow.tsx
+++ src/components/MessageBoard/MessageRow.tsx
@@ -5,18 +5,12 @@ export function MessageRow({ message }: { message: Message }) {
   const formattedDate = formatDate(message.createdAt);
 
   return (
-    <div className="p-4">
-      <div className="border-1 shadow sm:rounded-lg">
-        <div className="p-6">
-          <div>
-            <pre className="font-sans whitespace-pre-line">
-              {message.content}
-            </pre>
-          </div>
-          <div className="flex justify-between italic mt-2">
-            <span>{formattedDate}</span>
-            <span>{message.author}</span>
-          </div>
+    <div className="p-4 box-border">
+      <div className="border-1 shadow p-6 sm:rounded-lg">
+        <pre className="font-sans whitespace-pre-line">{message.content}</pre>
+        <div className="flex justify-between italic mt-2">
+          <span>{formattedDate}</span>
+          <span>{message.author}</span>
         </div>
       </div>
     </div>
